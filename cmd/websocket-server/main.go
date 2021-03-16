7ca283cff6cc43affadd725797452cb87b6e9547
huanghao <huanghao@yy.com>
2021-03-16 15:32:04 +0800

    use go-micro web to provide portal

--- cmd/websocket-server/main.go
+++ cmd/websocket-server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2/web"
+)
 
 func main() {
-	panic(http.ListenAndServe(":9100", http.FileServer(http.Dir("./html"))))
+	r := mux.NewRouter()
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./html")))
+
+	service := web.NewService(
+		web.Name("com.example.portal"),
+		web.Address(":9100"),
+		web.Handler(r),
+	)
+
+	service.Init()
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
